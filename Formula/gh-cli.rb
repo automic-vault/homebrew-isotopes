828b32112a0edd229ec18dd8ef8e0fffc0173f89
Max Howell <mxcl@me.com>
2026-07-12 09:49:14 -0400

    Refresh gh-cli isotope checksum

--- Formula/gh-cli.rb
+++ Formula/gh-cli.rb
@@ -2,7 +2,7 @@ class GhCli < Formula
   desc "Automic Vault build of GitHub CLI"
   homepage "https://github.com/automic-vault/gh-cli"
   url "https://github.com/automic-vault/gh-cli/releases/download/v2.96.0/cli-2.96.0.tgz"
-  sha256 "e3fe08853fafc00d395be06cacac0a550b0b27b8b4c5f6aa37082dbe71234844"
+  sha256 "4e7354587aab1a156d2adb71ec536d696ea721779251352c86ea6ae32a5c0117"
   license "MIT"
   conflicts_with "gh", because: "both install `gh`"
 
