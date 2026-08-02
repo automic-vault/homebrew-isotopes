1510aa39381625966cfdd44800858140ffc358c6
Max Howell <mxcl@me.com>
2026-08-02 11:58:30 -0400

    Update Stripe CLI isotope to v1.45.0

--- Formula/stripe-cli.rb
+++ Formula/stripe-cli.rb
@@ -1,8 +1,8 @@
 class StripeCli < Formula
   desc "Automic Vault build of Stripe CLI"
   homepage "https://github.com/automic-vault/stripe-cli"
-  url "https://github.com/automic-vault/stripe-cli/releases/download/v1.44.1/cli-1.44.1.tgz"
-  sha256 "c953fd749b994b4f82354b4e6e4ec3d2ac7d18b7d39d1b44786d8924378d7ea8"
+  url "https://github.com/automic-vault/stripe-cli/releases/download/v1.45.0/cli-1.45.0.tgz"
+  sha256 "f227fbab2fda02dbb32bf5c932987413e57045aca309f56161e458ca2164f43b"
   license "MIT"
   conflicts_with "stripe", because: "both install `stripe`"
 
