ef3738a1410da53ac8630570986db1d01e0fed6d
Max Howell <mxcl@me.com>
2026-08-12 13:45:16 -0400

    Update Automic Vault cask to 3.4.0

--- Casks/automic-vault.rb
+++ Casks/automic-vault.rb
@@ -1,6 +1,6 @@
 cask "automic-vault" do
-  version "3.3.0"
-  sha256 "51dc69d42cbbd2e3d70e101c2b1d81e7699c2fe9dc0763234277260e49e82f6e"
+  version "3.4.0"
+  sha256 "6197605ca244c996cf0db5d88ae2f3b99cddcf94d27aec7be96aa77475c30cce"
 
   url "https://github.com/automic-vault/automic-vault/releases/download/#{version}/Automic-Vault-#{version}.dmg"
   name "Automic Vault"
