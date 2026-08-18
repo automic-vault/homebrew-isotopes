5f5efdda9e53c37636e1768f67e3f1b476e2e345
Max Howell <mxcl@me.com>
2026-08-18 14:47:41 -0400

    Update Supabase CLI isotope to 2.115.0

--- Formula/supabase-cli.rb
+++ Formula/supabase-cli.rb
@@ -1,8 +1,8 @@
 class SupabaseCli < Formula
   desc "Automic Vault build of Supabase CLI"
   homepage "https://github.com/automic-vault/supabase-cli"
-  url "https://github.com/automic-vault/supabase-cli/releases/download/v2.114.0/cli-2.114.0.tgz"
-  sha256 "5a8926fea00ed2c69b1c315caaa9e842e59b05c49091f65e93f25f2f844bc813"
+  url "https://github.com/automic-vault/supabase-cli/releases/download/v2.115.0/cli-2.115.0.tgz"
+  sha256 "f7adeca7dcc7d44e2a143f9986cdaaafdd01699813e3b1dad01b6baa3f9ed1b6"
   license "MIT"
   conflicts_with "supabase", because: "both install `supabase`"
 
