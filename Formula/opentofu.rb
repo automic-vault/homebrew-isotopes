e23f510b1759bdd9579e640c63970ad3dc621b03
Max Howell <mxcl@me.com>
2026-08-24 11:39:15 -0400

    Add OpenTofu isotope formula

---
+++ Formula/opentofu.rb
@@ -0,0 +1,16 @@
+class Opentofu < Formula
+  desc "Automic Vault build of OpenTofu"
+  homepage "https://github.com/automic-vault/opentofu"
+  url "https://github.com/automic-vault/opentofu/releases/download/v1.12.6/cli-1.12.6.tgz"
+  sha256 "9ee77f53a4f8952db10ce35dedf05ad9da05ff380f812560db17d8ee2a4f6210"
+  license "MPL-2.0"
+  conflicts_with "opentofu", because: "both install `tofu`"
+
+  def install
+    bin.install "tofu"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tofu version")
+  end
+end
