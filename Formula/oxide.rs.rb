d65a7f4c227b9dbc017e141c1581a718a76c54f9
Max Howell <mxcl@me.com>
2026-08-24 11:28:20 -0400

    Add oxide.rs isotope formula

---
+++ Formula/oxide.rs.rb
@@ -0,0 +1,16 @@
+class OxideRs < Formula
+  desc "Automic Vault build of oxide"
+  homepage "https://github.com/automic-vault/oxide.rs"
+  url "https://github.com/automic-vault/oxide.rs/releases/download/v0.18.0+2026073100.0.0/cli-0.18.0+2026073100.0.0.tgz"
+  sha256 "98b89bbdbb6343b984e676857b237414b3e7bc6dfad7302174aded453bf808ba"
+  license "MPL-2.0"
+  conflicts_with "oxide-cli", because: "both install `oxide`"
+
+  def install
+    bin.install "oxide"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/oxide version")
+  end
+end
