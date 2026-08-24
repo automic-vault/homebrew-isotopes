56cb00fbce0d533c0a1bf62064bf2f8db72eaea6
Max Howell <mxcl@me.com>
2026-08-24 10:26:43 -0400

    Add plumber isotope formula

---
+++ Formula/plumber.rb
@@ -0,0 +1,16 @@
+class Plumber < Formula
+  desc "Automic Vault build of Plumber"
+  homepage "https://github.com/automic-vault/plumber"
+  url "https://github.com/automic-vault/plumber/releases/download/v2.9.0/cli-2.9.0.tgz"
+  sha256 "ebe647625d8674cca37b1a833fad875132761f6e7a018236459aa8555a3c7bcb"
+  license "MIT"
+  conflicts_with "plumber", because: "both install `plumber`"
+
+  def install
+    bin.install "plumber"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/plumber --version")
+  end
+end
