724db21b4d7772b5ed78a835e8fb0d708a0369a8
Max Howell <mxcl@me.com>
2026-08-25 14:12:28 -0400

    Update railway-cli isotope to 5.43.4

--- Formula/railway-isotope.rb
+++ Formula/railway-isotope.rb
@@ -1,8 +1,8 @@
 class RailwayIsotope < Formula
   desc "Automic Vault build of Railway CLI"
   homepage "https://github.com/automic-vault/railway-cli"
-  url "https://github.com/automic-vault/railway-cli/releases/download/v5.43.2/cli-5.43.2.tgz"
-  sha256 "cc17c953839082f3afac6baa17ccf27691b1d8f768e3a504debf77d3e30a1f61"
+  url "https://github.com/automic-vault/railway-cli/releases/download/v5.43.4/cli-5.43.4.tgz"
+  sha256 "465ed29e6f487ea38b053dcc4731b58b9ee6281023236d834dfcc1d44613a46c"
   license "MIT"
   conflicts_with "railway", because: "both install `railway`"
 
