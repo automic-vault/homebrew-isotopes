cc5826a18dad63d07ca8b268a5b176bd627437bd
Max Howell <mxcl@me.com>
2026-08-27 15:11:20 -0400

    Update uaa-cli isotope to 0.22.0

--- Formula/uaa-cli-isotope.rb
+++ Formula/uaa-cli-isotope.rb
@@ -1,8 +1,8 @@
 class UaaCliIsotope < Formula
   desc "Automic Vault build of UAA CLI"
   homepage "https://github.com/automic-vault/uaa-cli"
-  url "https://github.com/automic-vault/uaa-cli/releases/download/v0.21.0/cli-0.21.0.tgz"
-  sha256 "83351f39b9c664f9cc4671a6b714fec919aa2c1fa034112f11329d83c97cb399"
+  url "https://github.com/automic-vault/uaa-cli/releases/download/v0.22.0/cli-0.22.0.tgz"
+  sha256 "79c44b43aa665362e40f0fcf70772b433d077e251dd3e13e8e445c052a0e0d7b"
   license "Apache-2.0"
   def install
     bin.install "uaa"
