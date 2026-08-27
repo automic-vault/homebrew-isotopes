f5c4b86c12eca774851171919e36648508d9749a
Max Howell <mxcl@me.com>
2026-08-27 18:14:44 -0400

    Update rclone isotope to 1.75.0

--- Formula/rclone-isotope.rb
+++ Formula/rclone-isotope.rb
@@ -2,7 +2,7 @@ class RcloneIsotope < Formula
   desc "Automic Vault build of rclone"
   homepage "https://github.com/automic-vault/rclone"
   url "https://github.com/automic-vault/rclone/releases/download/v1.75.0/cli-1.75.0.tgz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "fb43f74abde1cf2ddcd5a3508b05155d2e2d22182a7bfa758be88bd059b75744"
   license "MIT"
   conflicts_with "rclone", because: "both install `rclone`"
 
