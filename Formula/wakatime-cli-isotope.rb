8168c88bafc9452600112e2add102d59d41017ff
Max Howell <mxcl@me.com>
2026-08-30 15:07:31 -0400

    Update wakatime-cli isotope to 2.26.0

--- Formula/wakatime-cli-isotope.rb
+++ Formula/wakatime-cli-isotope.rb
@@ -1,8 +1,8 @@
 class WakatimeCliIsotope < Formula
   desc "Automic Vault build of WakaTime CLI"
   homepage "https://github.com/automic-vault/wakatime-cli"
-  url "https://github.com/automic-vault/wakatime-cli/releases/download/v2.25.0/cli-2.25.0.tgz"
-  sha256 "58d80447b34e969432659ab98bfa55be20f0db00c6e9f20b40aca7eb8ef883a3"
+  url "https://github.com/automic-vault/wakatime-cli/releases/download/v2.26.0/cli-2.26.0.tgz"
+  sha256 "99703dfc20b0ea957004c6c68d3c0cc00a5f21fcd55fef7b10186f27b1984663"
   license "BSD-3-Clause"
   conflicts_with "wakatime-cli", because: "both install `wakatime-cli`"
 
