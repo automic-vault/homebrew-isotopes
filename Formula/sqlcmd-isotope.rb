06c19b77473f6e326c1177f59f798b01b126f2b0
Max Howell <mxcl@me.com>
2026-09-01 22:19:30 -0400

    Update go-sqlcmd isotope to 1.10.0

--- Formula/sqlcmd-isotope.rb
+++ Formula/sqlcmd-isotope.rb
@@ -2,7 +2,7 @@ class SqlcmdIsotope < Formula
   desc "Automic Vault build of Microsoft sqlcmd"
   homepage "https://github.com/automic-vault/go-sqlcmd"
   url "https://github.com/automic-vault/go-sqlcmd/releases/download/v1.10.0/cli-1.10.0.tgz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "350ea793e62622d20fa3f7b81bc2e25df4c859493834e97981f4117a0269add9"
   license "MIT"
   conflicts_with "sqlcmd", because: "both install `sqlcmd`"
 
