51cea7213bfdf5369eb332f9ebb0cadfff2298c5
Max Howell <mxcl@me.com>
2026-09-09 15:38:45 -0400

    Update aliyun-cli isotope to 3.5.0

--- Formula/aliyun-cli-isotope.rb
+++ Formula/aliyun-cli-isotope.rb
@@ -1,8 +1,8 @@
 class AliyunCliIsotope < Formula
   desc "Automic Vault build of Alibaba Cloud CLI"
   homepage "https://github.com/automic-vault/aliyun-cli"
-  url "https://github.com/automic-vault/aliyun-cli/releases/download/v3.4.11/cli-3.4.11.tgz"
-  sha256 "25d9dce38b11b2ab0e1ffb91cc0f19134502f4f3c1cf947ccff438663d61468e"
+  url "https://github.com/automic-vault/aliyun-cli/releases/download/v3.5.0/cli-3.5.0.tgz"
+  sha256 "b73a0374f17dee8058348b1340df235b26da848a1f4f9dca217b3a765067d69e"
   license "Apache-2.0"
 
   def install
