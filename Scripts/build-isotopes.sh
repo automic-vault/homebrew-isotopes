fee77b0babc2399070a289244f9f8cf9792d3a00
Max Howell <mxcl@me.com>
2026-09-09 15:12:37 -0400

    Map Wrangler to its isotope formula

--- Scripts/build-isotopes.sh
+++ Scripts/build-isotopes.sh
@@ -381,6 +381,7 @@ formula_name() {
     go-sqlcmd) echo sqlcmd-isotope ;;
     goat) echo goat-isotope ;;
     railway-cli) echo railway-isotope ;;
+    wrangler) echo wrangler-isotope ;;
     ordercli) echo ordercli-isotope ;;
     uaa-cli) echo uaa-cli-isotope ;;
     openhue-cli) echo openhue-cli-isotope ;;
