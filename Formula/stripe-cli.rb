f60eff357b1341b4322aa33f5bab74ef3b7a6700
Max Howell <mxcl@me.com>
2026-09-10 08:30:32 -0400

    Avoid legacy Stripe trust indirection

--- Formula/stripe-cli.rb
+++ Formula/stripe-cli.rb
@@ -4,7 +4,6 @@ class StripeCli < Formula
   url "https://github.com/automic-vault/stripe-cli/releases/download/v1.50.10/cli-1.50.10.tgz"
   sha256 "63ee73e721ece65507b0428de8b65a7a4511404e0a34025f37559b9668537c23"
   license "MIT"
-  conflicts_with "stripe-isotope", because: "both install `stripe`"
 
   def install
     bin.install "stripe"
