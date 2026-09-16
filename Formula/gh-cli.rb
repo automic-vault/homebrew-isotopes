f237681099ea07955323907cb8f6218968c899c6
Max Howell <mxcl@me.com>
2026-09-16 11:49:51 -0400

    Update gh-cli isotope to 2.101.0-1

--- Formula/gh-cli.rb
+++ Formula/gh-cli.rb
@@ -1,8 +1,8 @@
 class GhCli < Formula
   desc "Automic Vault build of GitHub CLI"
   homepage "https://github.com/automic-vault/gh-cli"
-  url "https://github.com/automic-vault/gh-cli/releases/download/v2.101.0/cli-2.101.0.tgz"
-  sha256 "b9347094e64bf8a294e2cb2350f400702f2f175931f76dd5b7fd82e24e4fa3ea"
+  url "https://github.com/automic-vault/gh-cli/releases/download/v2.101.0-1/cli-2.101.0-1.tgz"
+  sha256 "1fcf19a4211e7da3f28c0a8477609618a87bd0680338e1cd0ab3d884d9c93833"
   license "MIT"
   conflicts_with "gh", because: "both install `gh`"
 
