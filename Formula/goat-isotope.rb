cc95da2ba263620248fc9068f9c73470258ad8a7
Max Howell <mxcl@me.com>
2026-09-23 15:26:20 -0400

    Update goat isotope to 0.2.5

--- Formula/goat-isotope.rb
+++ Formula/goat-isotope.rb
@@ -1,8 +1,8 @@
 class GoatIsotope < Formula
   desc "Automic Vault build of goat"
   homepage "https://github.com/automic-vault/goat"
-  url "https://github.com/automic-vault/goat/releases/download/v0.2.4/cli-0.2.4.tgz"
-  sha256 "5bed9897b83a53b348765c3d6e327e3eac35e15e6ace56a495049fd56f5bf5e6"
+  url "https://github.com/automic-vault/goat/releases/download/v0.2.5/cli-0.2.5.tgz"
+  sha256 "42d34e3ca21ea4c4f33804f598350b6263e0a95d8cc3d7edb4da657f39b06116"
   license "Apache-2.0"
   conflicts_with "goat", because: "both install `goat`"
 
