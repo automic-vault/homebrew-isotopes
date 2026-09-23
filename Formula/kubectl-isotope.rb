49d9f534edc9f7b80aa79a55acb3597c3954d713
Max Howell <mxcl@me.com>
2026-09-23 15:22:48 -0400

    Update kubectl isotope to 1.37.1

--- Formula/kubectl-isotope.rb
+++ Formula/kubectl-isotope.rb
@@ -1,8 +1,8 @@
 class KubectlIsotope < Formula
   desc "Automic Vault build of kubectl"
   homepage "https://github.com/automic-vault/kubectl"
-  url "https://github.com/automic-vault/kubectl/releases/download/v1.37.0/cli-1.37.0.tgz"
-  sha256 "897afbaff4a1cf9fcf08df574e86c94d0e47a6ca0903fd1830176eaec30096ae"
+  url "https://github.com/automic-vault/kubectl/releases/download/v1.37.1/cli-1.37.1.tgz"
+  sha256 "a1c031f608ae7795104dac98b6bc4628bc0a353b7efe11c9fb4db37d4c0e3690"
   license "Apache-2.0"
   conflicts_with "kubernetes-cli", because: "both install `kubectl`"
 
